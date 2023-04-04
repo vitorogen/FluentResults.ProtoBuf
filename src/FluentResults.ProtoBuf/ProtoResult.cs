af69e101ac4c6384bae8723145bf8b7681c65756
Vitor <vitor.ogen@gmail.com>
2023-04-04 18:16:44 -0300

    Added support to generic Results.

--- src/FluentResults.ProtoBuf/ProtoResult.cs
+++ src/FluentResults.ProtoBuf/ProtoResult.cs
@@ -5,10 +5,116 @@ using System.Collections.Generic;
 namespace FluentResults.ProtoBuf;
 
 /// <summary>
+/// Represents an interface for ProtoResult and ProtoResult&lt;TValue&gt;.
+/// </summary>
+public interface IProtoResult
+{
+    /// <summary>
+    /// Gets or sets the list of reasons for the result.
+    /// </summary>
+    List<ProtoReason> Reasons { get; set; }
+}
+
+[ProtoContract]
+public class ProtoResult<TValue> : IProtoResult
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ProtoResult" /> class.
+    /// </summary>
+    public ProtoResult()
+    {
+        this.Reasons = new List<ProtoReason>();
+    }
+
+    /// <summary>
+    ///     Gets or sets the list of reasons for the result.
+    /// </summary>
+    [ProtoMember(1)]
+    public List<ProtoReason> Reasons { get; set; }
+
+    [ProtoMember(2)]
+    public TValue Value { get; set; }
+
+    /// <summary>
+    /// Converts a Result&lt;TValue&gt; object to a ProtoResult&lt;TValue&gt; object.
+    /// </summary>
+    /// <param name="result">The Result&lt;TValue&gt; object to convert.</param>
+    /// <returns>The converted ProtoResult&lt;TValue&gt; object.</returns>
+    public static implicit operator ProtoResult<TValue>(Result<TValue> result)
+    {
+        var protoResult = new ProtoResult<TValue>
+        {
+            Value = result.Value
+        };
+
+        foreach (var reason in result.Reasons)
+        {
+            switch (reason)
+            {
+                case Error error:
+                    protoResult.Reasons.Add((ProtoError)error);
+                    break;
+                case Success success:
+                    protoResult.Reasons.Add((ProtoSuccess)success);
+                    break;
+                default:
+                    throw new InvalidProtoReasonException("Reason is not of type Error or Success");
+            }
+        }
+
+        return protoResult;
+    }
+
+
+    /// <summary>
+    /// Converts a TValue object to a ProtoResult&lt;TValue&gt; object.
+    /// </summary>
+    /// <param name="value">The TValue object to convert.</param>
+    /// <returns>The converted ProtoResult&lt;TValue&gt; object.</returns>
+    public static implicit operator ProtoResult<TValue>(TValue value)
+    {
+        var protoResult = new ProtoResult<TValue>
+        {
+            Value = value
+        };
+        return protoResult;
+    }
+
+    public Result<TValue> ToResult()
+    {
+        var result = new Result<TValue>();
+
+        foreach (var protoReason in this.Reasons)
+        {
+            switch (protoReason)
+            {
+                case ProtoError protoError:
+                    result.WithError((Error)protoError);
+                    break;
+                case ProtoSuccess protoSuccess:
+                    result.WithSuccess((Success)protoSuccess);
+                    break;
+                default:
+                    throw new InvalidProtoReasonException("Invalid ProtoReason type encountered.");
+            }
+        }
+
+        if (result.IsFailed)
+        {
+            return result;
+        }
+
+        result = result.WithValue(this.Value);
+        return result;
+    }
+}
+
+
+/// <summary>
 ///     Represents a serializable result that can contain a list of reasons, either errors or successes.
 /// </summary>
 [ProtoContract]
-public class ProtoResult
+public class ProtoResult : IProtoResult
 {
     /// <summary>
     ///     Initializes a new instance of the <see cref="ProtoResult" /> class.
@@ -41,7 +147,7 @@ public class ProtoResult
                     protoResult.Reasons.Add((ProtoSuccess)success);
                     break;
                 default:
-                    throw new InvalidCastException("Reason is not of type Error or Success");
+                    throw new InvalidProtoReasonException("Reason is not of type Error or Success");
             }
 
         return protoResult;
--- tests/FluentResults.ProtoBuf.UnitTests/ProtoResultTests.cs
+++ tests/FluentResults.ProtoBuf.UnitTests/ProtoResultTests.cs
@@ -165,4 +165,148 @@ public sealed class ProtoResultTests
         // Assert
         deserializedProtoResult.Should().BeEquivalentTo(protoResult);
     }
+
+    [Fact]
+    public void ToResult_WithEmptyProtoResultWithValue_ShouldReturnEmptyResultWithValue()
+    {
+        // Arrange
+        var protoResult = new ProtoResult<int> { Value = 42 };
+
+        // Act
+        var result = protoResult.ToResult();
+
+        // Assert
+        result.Reasons.Should().BeEmpty();
+        result.Value.Should().Be(42);
+    }
+
+    [Fact]
+    public void ToResult_WithProtoResultWithValueHavingSuccessReason_ShouldReturnResultWithValueAndSuccess()
+    {
+        // Arrange
+        var protoResult = new ProtoResult<int> { Value = 42 };
+        protoResult.Reasons.Add(new ProtoSuccess("Success!"));
+
+        // Act
+        var result = protoResult.ToResult();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(42);
+    }
+
+    [Fact]
+    public void ToResult_WithProtoResultWithValueHavingErrorReason_ShouldReturnResultWithValueAndError()
+    {
+        // Arrange
+        var protoResult = new ProtoResult<int> { Value = 42 };
+        protoResult.Reasons.Add(new ProtoError("Error!"));
+
+        // Act
+        var result = protoResult.ToResult();
+        var getValueAction = () => result.Value;
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        getValueAction.Should().Throw<InvalidOperationException>();
+    }
+
+
+    [Fact]
+    public void Given_ProtoResultWithValue_When_SerializedAndDeserialized_Then_ShouldPreserveValue()
+    {
+        // Arrange
+        var originalProtoResult = new ProtoResult<int> { Value = 42 };
+
+        // Act
+        var deserializedProtoResult = SerializeAndDeserialize(originalProtoResult);
+
+        // Assert
+        deserializedProtoResult.Value.Should().Be(42);
+    }
+
+    [Fact]
+    public void Given_ProtoResultWithValueAndSuccessReason_When_SerializedAndDeserialized_Then_ShouldPreserveValueAndReason()
+    {
+        // Arrange
+        var originalProtoResult = new ProtoResult<int> { Value = 42 };
+        originalProtoResult.Reasons.Add(new ProtoSuccess("Success!"));
+
+        // Act
+        var deserializedProtoResult = SerializeAndDeserialize(originalProtoResult);
+
+        // Assert
+        deserializedProtoResult.Value.Should().Be(42);
+        deserializedProtoResult.Reasons.Should().HaveCount(1);
+        deserializedProtoResult.Reasons[0].Should().BeOfType<ProtoSuccess>();
+        ((ProtoSuccess)deserializedProtoResult.Reasons[0]).Message.Should().Be("Success!");
+    }
+
+    [Fact]
+    public void Given_ProtoResultWithValueAndErrorReason_When_SerializedAndDeserialized_Then_ShouldPreserveValueAndReason()
+    {
+        // Arrange
+        var originalProtoResult = new ProtoResult<int> { Value = 42 };
+        originalProtoResult.Reasons.Add(new ProtoError("Error!"));
+
+        // Act
+        var deserializedProtoResult = SerializeAndDeserialize(originalProtoResult);
+
+        // Assert
+        deserializedProtoResult.Value.Should().Be(42);
+        deserializedProtoResult.Reasons.Should().HaveCount(1);
+        deserializedProtoResult.Reasons[0].Should().BeOfType<ProtoError>();
+        ((ProtoError)deserializedProtoResult.Reasons[0]).Message.Should().Be("Error!");
+    }
+
+    private static ProtoResult<TValue> SerializeAndDeserialize<TValue>(ProtoResult<TValue> originalProtoResult)
+    {
+        using var memoryStream = new MemoryStream();
+        Serializer.Serialize(memoryStream, originalProtoResult);
+        memoryStream.Position = 0;
+        return Serializer.Deserialize<ProtoResult<TValue>>(memoryStream);
+    }
+
+
+    [Fact]
+    public void Given_ResultWithValue_When_ImplicitlyConvertedToProtoResultWithValue_Then_ShouldPreserveValue()
+    {
+        // Arrange
+        var result = Result.Ok(42);
+
+        // Act
+        ProtoResult<int> protoResult = result;
+
+        // Assert
+        protoResult.Value.Should().Be(42);
+    }
+
+    [Fact]
+    public void Given_ResultWithValueAndReason_When_ImplicitlyConvertedToProtoResultWithValue_Then_ShouldPreserveValueAndReason()
+    {
+        // Arrange
+        var result = Result.Ok(42).WithSuccess("Success!");
+
+        // Act
+        ProtoResult<int> protoResult = result;
+
+        // Assert
+        protoResult.Value.Should().Be(42);
+        protoResult.Reasons.Should().HaveCount(1);
+        protoResult.Reasons[0].Should().BeOfType<ProtoSuccess>();
+        ((ProtoSuccess)protoResult.Reasons[0]).Message.Should().Be("Success!");
+    }
+
+    [Fact]
+    public void Given_Value_When_ImplicitlyConvertedToProtoResultWithValue_Then_ShouldSetProtoResultWithValue()
+    {
+        // Arrange
+        var value = 42;
+
+        // Act
+        ProtoResult<int> protoResult = value;
+
+        // Assert
+        protoResult.Value.Should().Be(42);
+    }
 }
